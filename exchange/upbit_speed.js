18a07c799f56af52f98fbcf6f1279e4e13a85415
pickmoment <pickmoment@gmail.com>
2018-01-04 09:34:40 +0900

    oh

--- exchange/upbit_speed.js
+++ exchange/upbit_speed.js
@@ -16,19 +16,34 @@ Vue.filter('currency', function(value) {
 Vue.component('coin-board', {
   props: ['coins'],
   template: `
-    <table>
+    <div class="row">
+    <div class="col s12 m9 l6">
+    <table class="bordered">
     <thead>
       <tr>
-        <th>코인</th>
-        <th>프리미엄</th>
-        <th>현재가</th>
-        <th>1일 %</th>
-        <th>최소 %</th>
-        <th>중간 %</th>
-        <th>최대 %</th>
-        <th>초당Tick</th>
-        <th>Bid율</th>
-        <th>초당Volume</th>
+        <th class="right-align">
+          <div>코인</div>
+        </th>
+        <th class="right-align">
+          <div>프리미엄</div>
+          <div>현재가</div>
+        </th>
+        <th class="right-align">
+          <div>1일 %</div>
+          <div>최소 %</div>
+        </th>
+        <th class="right-align">
+          <div>중간 %</div>
+          <div>최대 %</div>
+        </th>
+        <th class="right-align">
+          <div>초당Tick</div>
+          <div>Bid율</div>
+        </th>
+        <th class="right-align">
+          <div>초당Volume</div>
+          <div>시작시간</div>
+        </th>
       </tr>
     </thead>
     <tbody>
@@ -39,6 +54,8 @@ Vue.component('coin-board', {
       </coin-view>
     </tbody>
   </table>
+  </div>
+  </div>
 `        
 })
 
@@ -46,21 +63,35 @@ Vue.component('coin-view', {
   props: ['coin'],
   template: `
     <tr>
-      <td>{{coin.symbol}} <a v-bind:href="coin.coinmarketcap_link" target="_blank">({{coin.name}})</a></td>
-      <td>{{coin.premium | currency}}</td>
-      <td>{{coin.price | currency}}</td>
-      <td>{{coin.chnage_1d | currency}}</td>
-      <td>{{coin.change_min | currency}}</td>
-      <td>{{coin.change_median | currency}}</td>
-      <td>{{coin.change_max | currency}}</td>
-      <td>{{coin.speed | currency}}</td>
-      <td>{{coin.bidrate | currency}}</td>
-      <td>{{coin.volume_speed | currency}}</td>
+      <td class="right-align">
+        <div>{{coin.symbol}}</div> 
+        <div><a v-bind:href="coin.coinmarketcap_link" target="_blank">{{coin.name}}</a></div>
+      </td>
+      <td class="right-align">
+        <div>{{coin.premium | currency}}</div>
+        <div>{{coin.price | currency}}</div>
+      </td>
+      <td class="right-align">
+        <div>{{coin.chnage_1d | currency}}</div>
+        <div>{{coin.change_min | currency}}</div>
+      </td>
+      <td class="right-align">
+        <div>{{coin.change_median | currency}}</div>
+        <div>{{coin.change_max | currency}}</div>
+      </td>
+      <td class="right-align">
+        <div>{{coin.speed | currency}}</div>
+        <div>{{coin.bidrate | currency}}</div>
+      </td>
+      <td class="right-align">
+        <div>{{coin.volume_speed | currency}}</div>
+        <div>{{coin.start_timestamp | formatDate}}</div>
+      </td>
     </tr> 
   `        
 })
 
-TICK_COUNT = 500
+TICK_COUNT = 1000
 
 symbol_map = {
   'BCC': 'BCH'
@@ -109,6 +140,9 @@ var app = new Vue({
         this.fetchCoinMarketCap();
         this.timer_coinmarketcap = setInterval(this.fetchCoinMarketCap, 240000)
             
+        this.fetchCoinTicks()
+        this.timer = setInterval(this.fetchCoinTicks, 10000)
+      
       }, err => {
         console.log(err)
       })
@@ -126,19 +160,25 @@ var app = new Vue({
         // tick_list = []
         // console.log(data_coinmarketcap)
         if (tickers && tickers.length > 0) {
-          ticker = tickers[0]
-          code_part = ticker.code.split('.')
+          let ticker = tickers[0]
+          let code_part = ticker.code.split('.')
           code_part = code_part[2].split('-')
-          market = code_part[0]
-          symbol = code_part[1]
+          let market = code_part[0]
+          let symbol = code_part[1]
+
+          ticker.coin_name = ''
+          ticker.premium = 'N/A'
+          ticker.coinmarketcap_link = ''
 
           if (market === 'KRW') {
             coinmarket = this.data_coinmarketcap[findSymbol(symbol)]
-            coin_name = coinmarket.name
-            coin_name_code = coin_name.replace(' ', '-')
-            coin_name_code = coin_name_code.toLowerCase()
-            ticker.premium = ((ticker.tradePrice / coinmarket.price - 1) * 100).toFixed(2)
-            ticker.coinmarketcap_link = `https://coinmarketcap.com/currencies/${coin_name_code}/#markets`
+            if (coinmarket) {
+              ticker.coin_name = coinmarket.name
+              coin_name_code = ticker.coin_name.replace(' ', '-')
+              coin_name_code = coin_name_code.toLowerCase()
+              ticker.premium = ((ticker.tradePrice / coinmarket.price - 1) * 100).toFixed(2)
+              ticker.coinmarketcap_link = `https://coinmarketcap.com/currencies/${coin_name_code}/#markets`
+            }
             ticker.seconds = ((ticker.tradeTimestamp - tickers[tickers.length-1].tradeTimestamp) / 1000)
             ticker.speed = (TICK_COUNT / ticker.seconds).toFixed(2)
             ticker.bidrate = (tickers.filter(t => t.askBid === 'BID').length / TICK_COUNT * 100).toFixed(2)
@@ -147,37 +187,40 @@ var app = new Vue({
             tradePrices.sort((a, b) => a - b)
             // let sum_tradePrice = tradePrices.reduce((previous, current) => current += previous);
             // let avg_tradePrice = sum_tradePrice / tradePrices.length
-            let median_tradePrice = (tradePrices[(tradePrices.length - 1) >> 1] + tradePrices[tradePrices.length >> 1]) / 2
-            let max_tradePrice = tradePrices[tradePrices.length-1]
-            let min_tradePrice = tradePrices[0]
+            ticker.median_tradePrice = (tradePrices[(tradePrices.length - 1) >> 1] + tradePrices[tradePrices.length >> 1]) / 2
+            ticker.max_tradePrice = tradePrices[tradePrices.length-1]
+            ticker.min_tradePrice = tradePrices[0]
 
             // let avg_change = ((ticker.tradePrice / avg_tradePrice - 1) * 100).toFixed(2)
-            let change_median = ((ticker.tradePrice / median_tradePrice - 1) * 100).toFixed(2)
-            let change_max = ((ticker.tradePrice / max_tradePrice - 1) * 100).toFixed(2)
-            let change_min = ((ticker.tradePrice / min_tradePrice - 1) * 100).toFixed(2)
+            ticker.change_median = ((ticker.tradePrice / ticker.median_tradePrice - 1) * 100).toFixed(2)
+            ticker.change_max = ((ticker.tradePrice / ticker.max_tradePrice - 1) * 100).toFixed(2)
+            ticker.change_min = ((ticker.tradePrice / ticker.min_tradePrice - 1) * 100).toFixed(2)
             // let change_1d = (Number(ticker.signedChangeRate) * 100).toFixed(2)
-            let change_1d = ((ticker.tradePrice / ticker.prevClosingPrice - 1) * 100).toFixed(2)
+            ticker.change_1d = ((ticker.tradePrice / ticker.prevClosingPrice - 1) * 100).toFixed(2)
 
-            let tradeVolumes = tickers.map(t => t.tradeVolume).reduce((a, b) => b += a)
-            let volume_speed = tradeVolumes / ticker.seconds
+            ticker.tradeVolumes = tickers.map(t => t.tradeVolume).reduce((a, b) => b += a)
+            ticker.volume_speed = (ticker.tradeVolumes / ticker.seconds).toFixed(5)
 
-            console.log(symbol, coin_name, ticker.premium, ticker.tradePrice, 
-              change_1d, change_min, change_median, change_max, 
-              tickers.length, ticker.seconds, ticker.speed, ticker.bidrate, volume_speed)
+            console.log(symbol, ticker.coin_name, ticker.premium, ticker.tradePrice, 
+              ticker.change_1d, ticker.change_min, ticker.change_median, ticker.change_max, 
+              tickers.length, ticker.seconds, ticker.speed, ticker.bidrate, ticker.volume_speed)
 
             new_data = {
               symbol: symbol,
-              name: coin_name,
+              name: ticker.coin_name,
               premium: ticker.premium,
               price: ticker.tradePrice,
-              chnage_1d: change_1d,
-              change_min: change_min,
-              change_median: change_median,
-              change_max: change_max,
+              chnage_1d: ticker.change_1d,
+              change_min: ticker.change_min,
+              change_median: ticker.change_median,
+              change_max: ticker.change_max,
               speed: ticker.speed,
               bidrate: ticker.bidrate,
-              volume_speed: volume_speed,
+              volume_speed: ticker.volume_speed,
+              seconds: ticker.seconds,
+              count: tickers.length,
               timestamp: ticker.tradeTimestamp,
+              start_timestamp: tickers[tickers.length-1].tradeTimestamp,
               coinmarketcap_link: ticker.coinmarketcap_link
             }
             
@@ -213,10 +256,6 @@ var app = new Vue({
           }
         }
 
-        if (!this.timer) {
-          this.fetchCoinTicks()
-          this.timer = setInterval(this.fetchCoinTicks, 10000)
-        }
       }, err => {
         console.log(err)
       })
